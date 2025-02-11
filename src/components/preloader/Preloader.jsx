c818dd55fd8109bb6447e7f84d185e07beddd57d
Michał Jaroch <kontakt@michaljaroch.pl>
2025-02-11 13:13:16 +0100

    Fixed the preloader

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -6,9 +6,17 @@ export default function Preloader() {
 
     window.addEventListener("load", function () {
         setTimeout(function () {
+            console.log("test");
             setPreloader(true);
         }, 1000);
     });
+
+    if (document.readyState === "complete") {
+        setTimeout(function () {
+            console.log("test");
+            setPreloader(true);
+        }, 1000);
+    }
 
     return (
         <div className={`preloader ${preloader && "loaded"}`}>
